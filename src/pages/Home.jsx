adc207d713609c95efef5bfdbd46727d60619faa
Asad Siddiqui <asad@howeneat.com>
2023-04-09 21:37:29 -0700

    text animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,19 @@ const Home = () => {
    <AnimatePresence>
     {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
-            <motion.header>
+            <motion.header {...slideAnimation("down")}>
                 <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain"></img>
             </motion.header>
+            <motion.div className="home-content" {...headContainerAnimation}>
+                <motion.div {...headTextAnimation}>
+                    <h1 className="head-text">
+                        LET'S<br className="xl:block hidden"/> DO IT.
+                    </h1>
+                </motion.div>
+                <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+                    <p className="max-w-md font-normal text=gray-600 text-base"> Create your own unique and exclusive shirt with our brand new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.</p>
+                </motion.div>
+            </motion.div>
         </motion.section>
     )}
     </AnimatePresence>
